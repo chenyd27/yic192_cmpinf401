6e3008b14e2c5e908bd157ebd7fe79b77b48f1af
Yida Chen <yic192@pitt.edu>
2022-09-25 22:44:16 -0400

    add

---
+++ Lab5/src/RollDiceLab.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.util.Random;
+
+public class RollDiceLab {
+    public static void main(String[] args) {
+        Random random = new Random();
+        boolean flag = true;
+        while (flag){
+            String input = JOptionPane.showInputDialog("please input the times of rolls");
+            int times = Integer.parseInt(input);
+            RollDice(times,random);
+            String ifContinue = JOptionPane.showInputDialog("Press 1 and then enter if you want to continue");
+            int cont = Integer.parseInt(ifContinue);
+            if(cont == 1) continue;
+            else flag = false;
+        }
+        System.out.println("Process end.");
+    }
+    public static void RollDice(int time, Random ran){
+        int[] timeCollection = new int[13];
+        for(int i = 0;i < time;i++){
+            int dice1 = ran.nextInt(6) + 1;
+            int dice2 = ran.nextInt(6) + 1;
+            timeCollection[dice2 + dice1] += 1;
+        }
+        for(int i = 2;i <= 12;i++){
+            double frac = timeCollection[i] * 1.0 / time;
+            System.out.println("Value " + i + " appear " + timeCollection[i] + " times. And its fraction is: " + frac);
+        }
+        System.out.println();
+    }
+}
