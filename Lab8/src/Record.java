c33bc0958c74598f0dc8df92e760f6237fff0088
Yida Chen <yic192@pitt.edu>
2022-11-06 22:31:53 -0500

    Lab 8

--- Lab8/src/Record.java
+++ Lab8/src/Record.java
@@ -1,42 +1,19 @@
 public class Record {
-    public Record next;
-    public Data data;
-
-    public Record(){
-        this.next = null;
-        this.data = null;
-    }
-    public Record(Data data){
-        this.next = null;
-        this.data = data;
-    }
-    public Record(Data data,Record next){
-        this.data = data;
-        this.next = next;
-    }
-}
-class Data{
     private String name;
-    private double raceTime;
-    public Data(){}
-    public Data(String name, double raceTime) {
+    private double time;
+
+    public Record(String name, double time) {
         this.name = name;
-        this.raceTime = raceTime;
+        this.time = time;
     }
+
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public double getTime() {
+        return this.time;
     }
 
-    public double getRaceTime() {
-        return raceTime;
-    }
-
-    public void setRaceTime(double raceTime) {
-        this.raceTime = raceTime;
-    }
 }
 
--- Lab8/src/RecordList.java
+++ Lab8/src/RecordList.java
@@ -2,42 +2,122 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class RecordList {
-    Record head;
-    int length;
-    public RecordList(){
-        head = new Record();
-        length = 0;
+
+    /**
+     * The node class represents a single list element with a data item and
+     * reference to the next node in the list
+     */
+    private class Node {
+        // data held by this node
+        Record data;
+        // reference to the next node in the list, or null if there is no such node
+        // (i.e. this is the last node in the list)
+        Node next;
+
+        /**
+         * Creates a node with the given data
+         * @param dataValue data to hold in this node
+         */
+        public Node(Record dataValue) {
+            next = null;
+            data = dataValue;
+        }
+
     }
-    public int add(String name,double race){
-        Data data = new Data(name,race);
-        Record record = new Record(data);
+
+    private Node head;
+
+    /**
+     * Initializes a linked list with a single "dummy" node (contains no real data)
+     * to simplify code for the case of an empty list
+     */
+    public RecordList() {
+        head = new Node(null);
+    }
+
+    /**
+     * Finds the index of the record with the given name in the list
+     * @param name name to search for
+     * @return index of name in list if found, -1 otherwise
+     */
+    public int indexOf(String name) {
+        Node currentNode = head;
         int index = 0;
-        length++;
+
+        // traverse through the list looking for our target node
+        while (currentNode.next != null && !currentNode.next.data.getName().equals(name)) {
+            currentNode = currentNode.next;
+            index++;
+        }
+
+        // we got to the end of the list without finding our target
+        if (currentNode.next == null) return -1;
+        else return index;
+    }
+
+    /**
+     * Adds the given item to the list, sorted by time (lowest to highest)
+     * @param data data to add
+     * @return the index the data was inserted at
+     */
+    public int add(Record data) {
+        // Replace this with your own code. You can use the indexOf() method as a guide.
+        Node node = new Node(data);
+        int index = 0;
         if(head.next == null){
-            head.next = record;
+            head.next = node;
             return index;
         }
-        Record currentNode = head;
-        while(currentNode.next != null && currentNode.next.data.getRaceTime() < race){
+        Node currentNode = head;
+        while(currentNode.next != null && currentNode.next.data.getTime() < data.getTime()){
             currentNode = currentNode.next;
             index++;
         }
         if(currentNode.next == null){
-            currentNode.next = record;
+            currentNode.next = node;
         }else{
-            record.next = currentNode.next;
-            currentNode.next = record;
+            node.next = currentNode.next;
+            currentNode.next = node;
         }
         return index;
+
+        // traverse to place to insert node (i.e. get currentNode to be the one
+        // immediately before where we want to insert -- the last node with a strictly
+        // lower time)
+
+
+        // add new node in the spot we've found
+
     }
-    public void writeToFileMethod(String url){
+
+    /**
+     * Prints out each record in the list, one per line
+     */
+    public void print() {
+        // note that we start from our first *real* node in this case (head.next, not
+        // our dummy head)
+        Node currentNode = head.next;
+        while (currentNode != null) {
+            System.out.println(currentNode.data.getName() + ", " + currentNode.data.getTime());
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    /**
+     * Write out the contents of the linked list to the given file, one entry per line
+     * @param filename name of the file to write the list to
+     */
+    public void writeToFile(String filename) {
+        // Add your own code here to write out the list contents to a file. You can use
+        // our file writing examples and the print() method above as a guide.
         try{
-            FileWriter fileWriter = new FileWriter(url);
-            Record currentNode = head.next;
+            FileWriter fileWriter = new FileWriter(filename);
+            Node currentNode = head.next;
             while (currentNode != null){
                 fileWriter.append(currentNode.data.getName());
                 fileWriter.append(",");
-                fileWriter.append(""+currentNode.data.getRaceTime());
+                fileWriter.append(""+currentNode.data.getTime());
                 fileWriter.append('\n');
                 currentNode = currentNode.next;
             }
@@ -45,14 +125,5 @@ public class RecordList {
         } catch (IOException e){
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        RecordList recordList = new RecordList();
-        System.out.println(recordList.add("Bob",10.5));
-        System.out.println(recordList.add("Alice",12.4));
-        System.out.println(recordList.add("Bill",14.0));
-        System.out.println(recordList.add("Amy",13.0));
-        recordList.writeToFileMethod("race.txt");
     }
 }
