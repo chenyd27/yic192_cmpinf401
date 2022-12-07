e31a828283b190ea86eb7e491b0e8767e5a46574
Yida Chen <yic192@pitt.edu>
2022-12-07 01:47:39 -0500

    add test

--- yic192_MenuManager_v3/src/MenuTester.java
+++ yic192_MenuManager_v3/src/MenuTester.java
@@ -6,8 +6,13 @@
 
 public class MenuTester {
     public static void main(String[] args) throws Exception{
-        MenuManager randomize = new MenuManager("data/dishes.txt");
-        Menu myMenu = randomize.randomMenu("test");
+        MenuManager menuManager = new MenuManager("data/dishes.txt");
+        Menu myMenu = menuManager.randomMenu("test");
+        Menu myMenuMin = menuManager.minCaloriesMenu("minTest");
+        Menu myMenuMax = menuManager.maxCaloriesMenu("minTest");
         System.out.println(myMenu.description()+"\nTotal calories\t"+ myMenu.totalCalories());
+        System.out.println(myMenuMin.description()+"\nTotal calories\t"+ myMenuMin.totalCalories());
+        System.out.println(myMenuMax.description()+"\nTotal calories\t"+ myMenuMax.totalCalories());
+
     }
 }
